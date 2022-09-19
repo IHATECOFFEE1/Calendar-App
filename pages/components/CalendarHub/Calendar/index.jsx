a83b0d0fd22026011912d79d2ff9d3e8109e44c0
Felipe Souza Dias <fe_souzadias@hotmail.com>
2022-09-19 14:10:36 -0400

    Project Structure

---
+++ pages/components/CalendarHub/Calendar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+export default function Calendar() {
+    return (
+        <div className={styles.calender}>
+            menu calender
+
+            <div>
+                calender
+            </div>
+        </div>
+    );
+}
---
+++ pages/components/LandingPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+import styles from "./index.module.scss";
+
+
+export default function LandingPage() {
+    return (
+        <Layout>
+            <div className={styles.landingPage}>
+                Landing Page
+            </div>
+        </Layout>
+    );
+}
