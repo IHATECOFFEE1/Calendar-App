3fe214649296035d939498286292f11b5e0ec414
Stephanie Ponton <steph.ponton@knights.ucf.edu>
2022-10-26 22:32:48 -0400

    merging

--- src/components/CalendarHub/Calendar/index.jsx
+++ src/components/CalendarHub/Calendar/index.jsx
@@ -41,19 +41,37 @@ const events = [
     },
 ];
 
-
 function App() {
-    //const [newEvent, setNewEvent] = useState({})
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" })
+    const [allEvents, setAllEvents] = useState(events)
 
-
-
-
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent])
+    }
 
     return (
-        <div className="App">
-            <Calendar localizer={localizer} events={events}
-                startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
-        </div>
+        <div className="App" >
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div>
+                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }}
+                    value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+                />
+                <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }}
+                    selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                <DatePicker placeholderText="End Date"
+                    selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
+            <Calendar
+                localizer={localizer}
+                events={allEvents}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500, margin: "50px" }} />
+        </div >
     );
 }
 
