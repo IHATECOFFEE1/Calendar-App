4e6dcb54b6cc1547185139199f99550c2ed7e834
Dlarosag <85146282+Dlarosag@users.noreply.github.com>
2022-11-26 17:56:47 -0500

    Improved NavBar and Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,15 @@ import styles from "./index.module.scss";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            Footer
+            <div className={styles.footText}>
+              Everyday is a class project of students from the University of Central Florida.
+            </div>
+            <div className={styles.footText}>
+              This project was made in fall 2022
+            </div>
+            <div className={styles.title}>
+              Everyday
+            </div>
         </footer>
     );
 }
