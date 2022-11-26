da46cf3caa8a5a6337d5218087835fa5013e22b8
Dlarosag <85146282+Dlarosag@users.noreply.github.com>
2022-11-26 16:31:47 -0500

    Added the About block

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -20,18 +20,32 @@ export default function LandingPage() {
 
 
     return (
+
+        
+        
         <Layout>
-            <div className={styles.landingPage}>         
 
 
-                <div >
-                <GoogleButton onClick={handleLoginWithGoogle} />
-
+             
+            <div className={styles.landingPage}>        
+                <div className={styles.aboutBlock}>
+                    <div className={styles.subTitle}>
+                        About<br />Everyday
+                    </div>
+                    <div className={styles.about}>
+                        Organize your week and plan your everyday expenses<br /><br />Everyday is a calendar that lets you plan your week and keep track of where you spend your money
+                    </div>
                 </div>
+
                 
-         
 
+                <div className={styles.logBlock}>
+                    <div className={styles.googleSign}>
+                        <GoogleButton onClick={handleLoginWithGoogle} />
+                    </div>
+                </div>
 
+            
             </div>
 
 
@@ -40,3 +54,15 @@ export default function LandingPage() {
         
     );
 }
+/*
+
+Bottom
+<div className={styles.googleSign}>
+                <GoogleButton onClick={handleLoginWithGoogle} />
+
+                </div>
+
+
+
+
+*/
