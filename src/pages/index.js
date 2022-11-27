df413f4d7dc8d6e3b4ca2a0d3e2819a82b17c18a
Dlarosag <85146282+Dlarosag@users.noreply.github.com>
2022-11-27 18:43:19 -0500

    Routing for landing page (#21)
    * Routing for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
 import CalendarHub from "../components/CalendarHub";
+import LandingPage from "../components/LandingPage";
+import Navbar from "../components/Navbar";
+import { UserAuth } from "../services/AuthContext";
 
 export default function Home() {
+
+  const { user} = UserAuth();
+
   return (
-    <CalendarHub />
+
+    <div>
+      {user?.displayName ? (
+                <div>
+                  <Navbar/>
+                  <CalendarHub/>
+              </div>) 
+                :
+                ( <div>
+                  <Navbar/>
+                <LandingPage />
+                </div>) 
+            }
+    </div>
   )
 }
