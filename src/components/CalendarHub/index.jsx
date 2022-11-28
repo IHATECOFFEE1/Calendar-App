7ac993b1ee45af5e6173967a713934355f27d3f0
Felipe Souza Dias <87622073+IHATECOFFEE1@users.noreply.github.com>
2022-11-27 19:54:51 -0500

    Css (#25)
    * Accounts

--- src/components/CalendarHub/index.jsx
+++ src/components/CalendarHub/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CalendarUI from "./Calendar";
 import styles from "./index.module.scss";
 
-
 export default function LandingPage() {
     return (
         <div className={styles.container}>
